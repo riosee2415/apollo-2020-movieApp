83a6e1235dc4d508b914126443f8f5fe80638853
4leaf.ysh <4leaf.ysh@gmail.com>
2020-07-20 00:17:18 +0900

    gql

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,30 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+
+  if (loading) {
+    return "Loading";
+  }
+
+  if (data && data.movie) {
+    return <div>{data.movie.title}</div>;
+  }
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,80 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #000000, #434343);
+  height: 45vh;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Author = styled.div`
+  margin-top: 30px;
+  height: 20px;
+  line-height: 23px;
+  font-size: 12.5px;
+  border: 1px solid #fff;
+  border-radius: 3px;
+  padding: 5px;
+  box-shadow: 5px 5px 3px #fff;
+`;
+
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+
+  return (
+    <Container>
+      <Header>
+        <Title>APOLLO PROJECT</Title>
+        <Subtitle>MOVIE LIST</Subtitle>
+        <Author>4LEAF SOFTWARE - DevHO</Author>
+      </Header>
+
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
+};
